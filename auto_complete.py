6897a846b43fc4032d53a61f886b62d580517e2c
Or Shahar <orshahar91@gmail.com>
2022-07-25 15:01:39 +0300

    add auto complete

---
+++ auto_complete.py
@@ -0,0 +1,10 @@
+import AutoCompleteData
+
+
+def get_best_k_completions(prefix: str) -> list[AutoCompleteData]:
+   words = prefix.split()
+   # for word in words:
+   #    if word.count() >= 3:
+   #       get
+   return
+
